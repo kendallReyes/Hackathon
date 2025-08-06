b09358ddf0beb675590f3a005a873d75919df114
Allison Blanco <allisonblancorodriguez@gmail.com>
2025-08-06 12:03:07 -0600

    Actualización de la vista

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,10 +51,11 @@
         </header>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
-                <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
-                    <h1>Bienvenidos</h1>
-                    <h2>Allison Blanco y Kendall Reyes</h2>
+                <div class="flex-1 p-6 pb-12 lg:p-20 text-[13px] leading-[20px] bg-white dark:bg-[#161615] text-black dark:text-[#EDEDEC] shadow-[inset_0_0_0_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0_0_0_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
+                    <h1 class="text-xl font-semibold mb-2">Bienvenidos</h1>
+                    <h2 class="text-lg font-medium">Allison Blanco y Kendall Reyes</h2>
                 </div>
+
                 <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
                     {{-- Laravel Logo --}}
                     <svg class="w-full text-[#F53003] dark:text-[#F61500] transition-all translate-y-0 opacity-100 max-w-none duration-750 starting:opacity-0 starting:translate-y-6" viewBox="0 0 438 104" fill="none" xmlns="http://www.w3.org/2000/svg">
